1a3e7f5c8501ce2778e8775887d61f3659c5faf1
bourrasque <stampixel.t@gmail.com>
2023-05-06 17:28:44 -0400

    documentations

---
+++ pyfanity/pyfanity.py
@@ -0,0 +1,17 @@
+"""
+Main file
+"""
+
+
+class Pyfanity:
+    def __init__(self):
+        ignoreLeetspeak = False
+
+    def sanitize_string(self) -> str:
+        pass
+
+    def get_profanity(self, s: str) -> str:
+        pass
+
+    def isprofane(self, s: str) -> bool:
+        pass
--- pyfanity/test.py
+++ pyfanity/test.py
@@ -1,5 +1,11 @@
 def add_one(number: int) -> int:
+    """
+    Increments a number by 1.
+
+    >>> add_one(9)
+    10
+
+    :param number: int
+    :return: int
+    """
     return number + 1
-
-
-
